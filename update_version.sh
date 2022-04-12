df67d3424496e8a41b02bb37ae96b433e65a09a9
Tim Sherratt <tim@discontents.com.au>
2022-04-12 12:16:24 +1000

    Modify script fro linux

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 repo_name=$(basename -s .git `git config --get remote.origin.url`)
 version_url="https://github.com/GLAM-Workbench/$repo_name/releases/tag/$1"
-sed -ri '_old' "s|Current version: \[.*\](.*tag\/v.*)|Current version: [$1]($version_url)|" README.md
+sed -ri "s|Current version: \[.*\](.*tag\/v.*)|Current version: [$1]($version_url)|" README.md
 text=$(sed '/^<\!-- START RUN INFO/,/^<\!-- END RUN INFO/d'  README.md | sed -r 's/\[(.*)\]\(.*\.ipynb\)/\1/' | pandoc | sed '1d' | echo $(cat));
 pdate=$(date +"%Y-%m-%d");
 identifier=$(jq -r '.related_identifiers[0].identifier' .zenodo.json | sed "s/\/tree\/v.*/\/tree\/$1/")
